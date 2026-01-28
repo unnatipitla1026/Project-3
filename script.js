eb61347b056d4464f019ace38132410cca41d48c
Unnati Pitla <unnatipitla1026@gmail.com>
2026-01-28 16:35:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Hamburger menu */
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
